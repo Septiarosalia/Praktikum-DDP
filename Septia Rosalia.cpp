335677dd32199b91723067a6287a537e2e819c68
Septiarosalia <112565985+Septiarosalia@users.noreply.github.com>
2022-09-08 10:06:59 +0700

    Add files via upload

---
+++ Septia Rosalia.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cin>>a>>b;
+	cout<<"keliling persegi panjang :" <<(2*a)+ (2*b)<<endl;
+	cout<<"luas persegi panjang :" <<a*b;
+	cin>>a;
+	cout<<"keliling persegi :"<<4*a<<endl<<"luas persegi   :  "<<a*a;
+	return 0;
+}
+
